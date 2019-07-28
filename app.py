3ada7dfe6baafc7abe562939aa42faf9d5acd22e
Eddie <webappvista@gmail.com>
2019-07-27 19:50:03 -0700

    conditional examples and notes

--- app.py
+++ app.py
@@ -1,2 +1,33 @@
-print("hello world")
-print('hello number 2')
+# temperature = 80
+# if temperature > 40:
+#     print("it's warm")
+#     print("Drink water")
+# elif temperature > 20:
+#     print("it's nice")
+# else:
+#     print("it's cold")
+# print("Done")
+
+# age = 2
+# message = "Eligible" if age >= 18 else "Not elibible"
+# print(message)
+
+# high_income = True
+# good_credit = False
+# studen = False
+
+# if (high_income or good_credit) and not studen:
+#     print("Eligible")
+# else:
+#     print("Not eligible")
+
+# age should be between 18 and 65
+
+# age = 65
+# if 18 <= age <= 65:
+#     print("Eligible")
+# else:
+#     print("Not eligible")
+
+# for i in range(1, 11, 2):
+#     print("Attempt", i, i * ".")
