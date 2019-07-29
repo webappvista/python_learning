5b0ba69526e4030d030f8413d52530482ad95eaf
Eddie <webappvista@gmail.com>
2019-07-28 18:47:32 -0700

    while loops

--- app.py
+++ app.py
@@ -32,19 +32,63 @@
 # for i in range(1, 11, 2):
 #     print("Attempt", i, i * ".")
 
-high_income = False
-good_credit = True
-student = True
+# high_income = False
+# good_credit = True
+# student = True
 
-if high_income and good_credit and student:
-    print("You are Approved and You get very low interest")
-elif high_income or good_credit and student:
-    print("Eligible but, high interest")
-else:
-    print("You are NOT approved")
+# if high_income and good_credit and student:
+#     print("You are Approved and You get very low interest")
+# elif high_income or good_credit and student:
+#     print("Eligible but, high interest")
+# else:
+#     print("You are NOT approved")
 
-age = 66
-if 18 <= age <= 65:
-    print('Eligible')
-else:
-    print('Not Eligible')
+# age = 66
+# if 18 <= age <= 65:
+#     print('Eligible')
+# else:
+#     print('Not Eligible')
+
+
+# For Loops section
+
+# for i in range(1, 11):
+#     print('Attempting ', i, i * '.')
+
+# for Else loop
+
+# succesful = False
+
+# for i in range(3):
+#     print('Attempting')
+#     if succesful:
+#         print('Successful')
+#         break
+# else:
+#     print('Attempted 3 time and failed')
+
+# Nested loops
+
+# for x in range(1, 6):
+#     for y in range(1, 6):
+#         print(f"({x}),({y})")
+
+# iterables
+# comments = ['hello', 'hi', 'hola', 'bien']
+
+# for a in comments:
+#     print(f"This is thi first iteration " + a)
+
+# While loops
+
+# number = 10000
+
+# while number > 0:
+#     print(number)
+#     number //= 2
+
+command = ""
+
+while command.lower() != "quite":
+    command = input(">")
+    print("Echoing", command)
